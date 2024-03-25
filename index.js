0d03e96f9d33a8e0d455bbb6d11a86a9b6a059f1
Farrrel Devara <farel199@yahoo.com>
2024-03-25 15:55:57 +0700

    setup mongo

--- index.js
+++ index.js
@@ -4,31 +4,30 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
-const books = [
-    {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
-    },
-    {
-      title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-  ];
 
 const typeDefs = `#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
   # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
+  type User {
+    _id: ID
+    name: String
+    username: String
+    email: String
+    password: String
+  }
+
+  type Follow {
+    _id: ID
+    followingId: ID
+    follwerId: ID
   }
 
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
+    users: [User]
   }
 `;
 
@@ -36,7 +35,7 @@ const typeDefs = `#graphql
 // This resolver retrieves books from the "books" array above.
 const resolvers = {
     Query: {
-      books: () => books,
+        users: () => users,
     },
   };
 
